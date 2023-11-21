d4c2516251cdf7e4985cff882043a1ede0b982c6
Marcos <marcosrolando.mr@gmail.com>
2023-11-21 20:37:08 -0300

    Remove second TCP connection to player handler (not needed because its not an entity)

--- src/main/scala/server/protocol/PlayerAccepter.scala
+++ src/main/scala/server/protocol/PlayerAccepter.scala
@@ -1,30 +1,18 @@
 package server.protocol
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
-import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Source
 import akka.stream.scaladsl.Tcp
-import akka.util.ByteString
-import akka.util.Timeout
 
-import java.time.Duration
 import scala.concurrent.Future
-import scala.util.Failure
-import scala.util.Success
 
 object PlayerAccepter {
   sealed trait Command
   final case class Accept(connection: Tcp.IncomingConnection) extends Command
-  final case class HandlerResponse(
-      ip: String,
-      port: Int,
-      connection: Tcp.IncomingConnection
-  ) extends Command
 
   def apply(): Behavior[Command] = {
     Behaviors.setup(ctx => {
       implicit val system = ctx.system
-      implicit val timeout = Timeout.create(Duration.ofSeconds(3))
 
       val connections
           : Source[Tcp.IncomingConnection, Future[Tcp.ServerBinding]] =
@@ -35,23 +23,9 @@ object PlayerAccepter {
 
       Behaviors.receiveMessage {
         case Accept(connection: Tcp.IncomingConnection) => {
-          val playerHandler = ctx.spawn(
-            PlayerHandler(),
+          ctx.spawn(
+            PlayerHandler(connection),
             s"playerHandler${connection.remoteAddress.getPort()}"
-          )
-          ctx.ask(playerHandler, PlayerHandler.GetIPInfo.apply) {
-            case Success(PlayerHandler.GetIPInfoResponse(ip, port)) =>
-              HandlerResponse(ip, port, connection)
-            case Failure(_) => HandlerResponse("error", 0, connection)
-          }
-          Behaviors.same
-        }
-
-        case HandlerResponse(ip, port, connection) => {
-          connection.handleWith(
-            Flow[ByteString]
-              .merge(Source.single(ByteString(s"$ip:$port\n")))
-              .take(1)
           )
           Behaviors.same
         }
--- src/main/scala/server/protocol/PlayerHandler.scala
+++ src/main/scala/server/protocol/PlayerHandler.scala
@@ -1,55 +1,36 @@
 package server.protocol
 
-import akka.actor.typed.ActorRef
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import akka.stream.scaladsl.Flow
 import akka.stream.scaladsl.Framing
-import akka.stream.scaladsl.Sink
 import akka.stream.scaladsl.Tcp
 import akka.util.ByteString
 import server.Sharding
 import server.domain.entities.Player
 
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.util.Failure
-import scala.util.Success
 
 object PlayerHandler {
   sealed trait Command
-  final case class GetIPInfo(replyTo: ActorRef[GetIPInfoResponse])
-      extends Command
-  final case class ServerBinding(
-      ip: String,
-      port: Int,
-      replyTo: ActorRef[GetIPInfoResponse]
-  ) extends Command
   final case class ConnectionClosed() extends Command
 
-  final case class GetIPInfoResponse(ip: String, port: Int)
-
-  def apply(): Behavior[Command] = {
+  def apply(connection: Tcp.IncomingConnection): Behavior[Command] = {
     Behaviors.setup { ctx =>
       implicit val system = ctx.system
 
-      val connectionListener = Tcp(system).bind("localhost", 0)
-      val serverBinding = connectionListener
-        .take(1)
-        .to(Sink.foreach { connection =>
-          val clientResponse = Flow[ByteString]
-            .via(
-              Framing.delimiter(ByteString("\n"), 256, allowTruncation = true)
-            )
-            .map(_.utf8String)
-            .map(_ + "!!!\n")
-            .map(ByteString(_))
-            .watchTermination() { (_, done) =>
-              done.onComplete(_ => ctx.self ! ConnectionClosed())
-            }
+      val clientResponse = Flow[ByteString]
+        .via(
+          Framing.delimiter(ByteString("\n"), 256, allowTruncation = true)
+        )
+        .map(_.utf8String)
+        .map(_ + "!!!\n")
+        .map(ByteString(_))
+        .watchTermination() { (_, done) =>
+          done.onComplete(_ => ctx.self ! ConnectionClosed())
+        }
 
-          connection.handleWith(clientResponse)
-        })
-        .run()
+      connection.handleWith(clientResponse)
 
       val player = Sharding().entityRefFor(
         Player.TypeKey,
@@ -57,24 +38,6 @@ object PlayerHandler {
       ) // TODO use random entityId
 
       Behaviors.receiveMessage {
-        case GetIPInfo(replyTo) => {
-          ctx.pipeToSelf(serverBinding) {
-            case Success(binding) =>
-              ServerBinding(
-                binding.localAddress.getHostString,
-                binding.localAddress.getPort,
-                replyTo
-              )
-            case Failure(_) => ServerBinding("", 0, replyTo)
-          }
-          Behaviors.same
-        }
-
-        case ServerBinding(ip, port, replyTo) => {
-          replyTo ! GetIPInfoResponse(ip, port)
-          Behaviors.same
-        }
-
         case ConnectionClosed() => {
           ctx.log.info("Closing connection!")
           player ! Player.PrintPosition()
