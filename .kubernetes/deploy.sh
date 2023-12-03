88bb3ee28f5bcc59962015c17582dda65f7bf97f
Marcos Rolando <marcosrolando.mr@gmail.com>
2023-12-03 14:37:13 -0300

    Add kafka retention policy

--- .kubernetes/deploy.sh
+++ .kubernetes/deploy.sh
@@ -10,7 +10,11 @@ fi
 
 if ! helm list -n "$NAMESPACE" | grep -q "$RELEASE_NAME"; then
     echo "Installing Helm release '$RELEASE_NAME' in namespace '$NAMESPACE'."
-    helm install --set listeners.client.protocol=PLAINTEXT \
+    helm install \
+        --set listeners.client.protocol=PLAINTEXT \
+        --set extraConfig="| 
+            log.retention.minutes=2
+            log.retention.check.interval.ms=30000" \
         "$RELEASE_NAME" oci://registry-1.docker.io/bitnamicharts/kafka \
         --namespace "$NAMESPACE"
 fi
