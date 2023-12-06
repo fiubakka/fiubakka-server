517d5168e7508023206c0c24ee8d4d0b1b78496e
Marcos <marcos.rolando@tiendanube.com>
2023-12-06 17:32:24 -0300

    Fix dev image

--- .docker/dev-entrypoint.sh
+++ .docker/dev-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 su - postgres -c "pg_ctl -D /var/lib/postgresql/data start"
-exec java -jar app.jar
+exec java \
+  -Dakka.cluster.seed-nodes.0=akka://game-system@127.0.0.1:25520 \
+  -Dakka.remote.artery.canonical.port=25520 \
+  -Dakka.remote.artery.bind.port=25520 \
+  -Dplayer-accepter.port=9090 \
+  -jar app.jar
