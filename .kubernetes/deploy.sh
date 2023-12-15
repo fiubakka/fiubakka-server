142b9f058bc07e502c9c2be4958b4d201adefe92
marcos <marcos.rolando@tiendanube.com>
2023-12-15 19:24:59 -0300

    Add postgresdb to prod (wip)

--- .kubernetes/deploy.sh
+++ .kubernetes/deploy.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
 NAMESPACE="game-system-1"
-RELEASE_NAME="fiubakka-kafka"
+KAFKA_RELEASE_NAME="fiubakka-kafka"
+POSTGRES_RELEASE_NAME="fiubakka-postgres"
 
 if ! kubectl get namespace "$NAMESPACE" &> /dev/null; then
     echo "Creating namespace '$NAMESPACE'."
     kubectl create namespace "$NAMESPACE"
 fi
 
-if ! helm list -n "$NAMESPACE" | grep -q "$RELEASE_NAME"; then
-    echo "Installing Helm release '$RELEASE_NAME' in namespace '$NAMESPACE'."
+if ! helm list -n "$NAMESPACE" | grep -q "$KAFKA_RELEASE_NAME"; then
+    echo "Installing Helm release '$KAFKA_RELEASE_NAME' in namespace '$NAMESPACE'."
     helm install \
         --set listeners.client.protocol=PLAINTEXT \
         --set extraConfig="| 
             log.retention.minutes=2
             log.retention.check.interval.ms=30000" \
-        "$RELEASE_NAME" oci://registry-1.docker.io/bitnamicharts/kafka \
+        "$KAFKA_RELEASE_NAME" oci://registry-1.docker.io/bitnamicharts/kafka \
+        --namespace "$NAMESPACE"
+fi
+
+if ! helm list -n "$NAMESPACE" | grep -q "$POSTGRES_RELEASE_NAME"; then
+    echo "Installing Helm release '$POSTGRES_RELEASE_NAME' in namespace '$NAMESPACE'."
+    helm install \
+        "$POSTGRES_RELEASE_NAME" oci://registry-1.docker.io/bitnamicharts/postgresql \
         --namespace "$NAMESPACE"
 fi
 
