b356ebdcfcfa78d7780581d67967d609fac55498
marcos <marcosrolando.mr@gmail.com>
2023-12-19 15:13:35 -0300

    Add same node shard allocation strategy for Player

--- src/main/scala/server/Sharding.scala
+++ src/main/scala/server/Sharding.scala
@@ -1,6 +1,7 @@
 package server
 
 import akka.actor.typed.ActorSystem
+import akka.cluster.sharding.external.ExternalShardAllocationStrategy
 import akka.cluster.sharding.typed.scaladsl.ClusterSharding
 import akka.cluster.sharding.typed.scaladsl.Entity
 import akka.persistence.typed.PersistenceId
@@ -18,14 +19,26 @@ object Sharding {
     )
   }
 
+  // We configure the Player to use the ExternalShardAllocationStrategy
+  // so that the first time the Shard is created it is allocated to the node that contains
+  // the assigned PlayerHandler for the player.
+  //
+  // TODO It's possible to configure the PlayerPersistor shard to live in the same node of the Player shard.
+  // See https://doc.akka.io/docs/akka/current/typed/cluster-sharding.html#colocate-shards
+  //
+  // See https://doc.akka.io/docs/akka/current/typed/cluster-sharding.html#external-shard-allocation
   def configure(system: ActorSystem[_]) = {
     sharding = Some(ClusterSharding(system))
 
-    Sharding().init(Entity(typeKey = Player.TypeKey) { entityCtx =>
-      Player(
-        entityCtx.entityId
+    Sharding().init(
+      Entity(typeKey = Player.TypeKey) { entityCtx =>
+        Player(
+          entityCtx.entityId
+        )
+      }.withAllocationStrategy(
+        ExternalShardAllocationStrategy(system, Player.TypeKey.name)
       )
-    })
+    )
 
     Sharding().init(Entity(typeKey = PlayerPersistor.TypeKey) { entityCtx =>
       PlayerPersistor(
