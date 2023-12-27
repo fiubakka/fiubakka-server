8216788fdeabb504258ffddbf9273783b54e4ff9
Ivan Soriano <isoriano@fi.uba.ar>
2023-12-27 19:58:03 -0300

    Add player init ready message type to protocol message map

--- src/main/scala/server/protocol/client/ProtocolMessageMap.scala
+++ src/main/scala/server/protocol/client/ProtocolMessageMap.scala
@@ -5,6 +5,7 @@ import protobuf.client.init.player_init.PBPlayerInit
 import protobuf.client.metadata.PBClientMessageType
 import protobuf.client.movement.player_movement.PBPlayerMovement
 import protobuf.server.chat.message.{PBPlayerMessage => PBServerPlayerMessage}
+import protobuf.server.init.player_init_ready.PBPlayerInitReady
 import protobuf.server.metadata.PBServerMessageType
 import protobuf.server.state.game_entity_state.PBGameEntityState
 import scalapb.GeneratedMessage
@@ -23,6 +24,7 @@ object ProtocolMessageMap {
   val serverMessageMap: Map[String, PBServerMessageType] =
     Map(
       PBGameEntityState.getClass.toString -> PBServerMessageType.PBGameEntityState,
-      PBServerPlayerMessage.getClass.toString -> PBServerMessageType.PBPlayerMessage
+      PBServerPlayerMessage.getClass.toString -> PBServerMessageType.PBPlayerMessage,
+      PBPlayerInitReady.getClass.toString -> PBServerMessageType.PBPlayerInitReady
     )
 }
