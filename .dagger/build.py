7ca7b39c716a67defa3b2f8fb5cece2fbb1128b0
marcos <marcosrolando.mr@gmail.com>
2024-03-17 14:28:42 -0300

    Rework pipeline

--- .dagger/build.py
+++ .dagger/build.py
@@ -3,7 +3,7 @@ import anyio
 import subprocess
 import os
 
-async def build():
+async def main():
     subprocess.run(["git", "submodule", "update", "--init"])
 
     for env_var in ["DOCKER_USER", "DOCKER_PASS", "DOCKER_REPO"]:
@@ -13,22 +13,20 @@ async def build():
 
     async with dagger.Connection() as client, anyio.create_task_group() as tg:
         password = client.set_secret("docker_password", os.environ["DOCKER_PASS"])
-        build_image = client.host().directory(".").docker_build(dockerfile="Dockerfile.build").with_registry_auth(
-            "docker.io",
-            os.environ["DOCKER_USER"],
-            password
-        )
-        await tg.start_soon(publish_image, build_image, "build-latest")
-        app_image = client.host().directory('.').docker_build(dockerfile=f"Dockerfile.{os.environ["ENV"]}").with_registry_auth(
-            "docker.io",
-            os.environ["DOCKER_USER"],
-            password
-        )
-        for tag in [commit_sha, f"{os.environ["ENV"]}-latest"]:
-            tg.start_soon(publish_image, app_image, tag)
+        base_image = build_image(client, "Dockerfile.build", password)
+        await publish_image(base_image, f"build-{commit_sha}")
+        app_image = build_image(client, f"Dockerfile.{os.environ["ENV"]}", password)
+        for tag in [commit_sha, "latest"]:
+            tg.start_soon(publish_image, app_image, f"{os.environ["ENV"]}-{tag}")
+
+def build_image(client: dagger.Client, image_path: str, password: dagger.Secret):
+    return client.host().directory(".").docker_build(dockerfile=image_path).with_registry_auth(
+        "docker.io",
+        os.environ["DOCKER_USER"],
+        password
+    )
 
 async def publish_image(image: dagger.Container, tag: str):
-    return image.publish(f"{os.environ["DOCKER_REPO"]}:{tag}")
+    await image.publish(f"{os.environ["DOCKER_REPO"]}:{tag}")
 
-
-anyio.run(build)
+anyio.run(main)
