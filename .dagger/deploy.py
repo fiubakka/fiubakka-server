d0d2a06e23b5cecdfcd7901f15d958143fa0d63f
Marcos Rolando <marcosrolando.mr@gmail.com>
2024-03-29 10:30:52 -0300

    Add prod deploy (wip)

--- .dagger/deploy.py
+++ .dagger/deploy.py
@@ -1,5 +1,6 @@
 import dagger
 import anyio
+import os
 
 def kubectl(container: dagger.Container) -> dagger.Container:
     return container.with_exec(shell("apt-get update && apt-get install -y curl")) \
@@ -10,24 +11,40 @@ def cloudflared(container: dagger.Container) -> dagger.Container:
     return container.with_exec(["mkdir", "-p", "--mode=0755", "/usr/share/keyrings"]) \
         .with_exec(shell("curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null")) \
         .with_exec(shell("echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared bookworm main' | tee /etc/apt/sources.list.d/cloudflared.list")) \
-        .with_exec(shell("apt-get update && apt-get install -y cloudflared systemctl")) \
+        .with_exec(shell("apt-get update && apt-get install -y cloudflared procps")) \
+        .with_exec(shell("curl -LO https://github.com/mmiranda/cfdtunnel/releases/download/v0.1.4/cfdtunnel_0.1.4_Linux_x86_64.tar.gz")) \
+        .with_exec(shell("tar -xvzf cfdtunnel_0.1.4_Linux_x86_64.tar.gz")) \
+        .with_exec(shell("mkdir /root/.cfdtunnel")) \
+        .with_exec(shell("mkdir /root/.kube")) \
         .with_exec(shell("echo '\
-            \n[Unit] \
-                \n\tDescription=Cloudflared for Kubernetes \
-                \n\tAfter=network.target \
-            \n[Service] \
-                \n\tType=simple \
-                \n\tExecStart=/usr/local/bin/cloudflared access tcp --hostname kubernetes.marcosrolando.uk --url 127.0.0.1:6443 \
-                \n\tRestart=always \
-                \n\tUser=root \
-            \n[Install] \
-                \n\tWantedBy=multi-user.target \
-            ' >/etc/systemd/system/k3spi.service")) \
-        .with_exec(shell("systemctl enable k3spi.service")) \
-        .with_exec(shell("systemctl start k3spi.service")) \
+            [k8s] \
+            \nhost = https://kubernetes.marcosrolando.uk \
+            \nport = 6443 \
+            \nenv = HTTPS_PROXY=socks5://127.0.0.1:6443' > /root/.cfdtunnel/config"
+        )) \
+        .with_exec(shell(f"echo 'apiVersion: v1 \
+            \nclusters: \
+            \n- cluster: \
+                \n    certificate-authority-data: {os.environ["K8S_SERVER_CA"]} \
+                \n    server: https://raspberrypi1:6443 \
+            \n  name: k3s-local-cluster \
+            \ncontexts: \
+            \n- context: \
+                \n    cluster: k3s-local-cluster \
+                \n    user: k3s-local-cluster \
+            \n  name: k3s-local-cluster \
+            \ncurrent-context: k3s-local-cluster \
+            \nkind: Config \
+            \npreferences: {{}} \
+            \nusers: \
+            \n- name: k3s-local-cluster \
+                \n  user: \
+                \n    client-certificate-data: {os.environ["K8S_CLIENT_CA"]} \
+                \n    client-key-data: {os.environ["K8S_CLIENT_KEY"]}' > /root/.kube/config")) \
+        .with_exec(shell("./cfdtunnel --profile k8s -- env HTTPS_PROXY=socks5://127.0.0.1:6443 kubectl get po -n fiubakka-server-1")) \
 
-def shell(cmd: str, background = False) -> list[str]:
-    return ["sh", "-c", cmd, "&"] if background else ["sh", "-c", cmd]
+def shell(cmd: str) -> list[str]:
+    return ["sh", "-c", cmd]
 
 
 async def main():
@@ -35,7 +52,6 @@ async def main():
         await client.container().from_("docker.io/debian:12.5-slim") \
             .with_(kubectl) \
             .with_(cloudflared) \
-            .with_exec(shell("ps -e")) \
             .stdout()
 
 anyio.run(main)
