688c8b9d2fad88c242c7bab8dfd6357249675dcc
Marcos Rolando <marcosrolando.mr@gmail.com>
2024-03-29 19:20:38 -0300

    Add default command to prod deploy

--- .dagger/deploy.py
+++ .dagger/deploy.py
@@ -56,6 +56,7 @@ async def main():
             .with_(cloudflared) \
             .with_entrypoint(["./cfdtunnel", "--profile", "k8s", "--"]) \
             .with_default_args(["env", "HTTPS_PROXY=socks5://127.0.0.1:6443", "kubectl", "get", "po", "-n", "fiubakka-server-1"]) \
+            .with_exec() \
             .stdout()
 
 anyio.run(main)
