77624f70dabbf5878a2fc45d3a23c19aa3456237
marcos <marcosrolando.mr@gmail.com>
2024-03-31 19:37:37 -0300

    Debug map update in prod

--- src/main/scala/server/protocol/event/kafka/KafkaConsumer.scala
+++ src/main/scala/server/protocol/event/kafka/KafkaConsumer.scala
@@ -78,7 +78,12 @@ object KafkaConsumer {
     // Do note that while we are not specifying the startAfterNrOfConsumers explicitly, the default is 0.
     // To solve this, we force a dummy consumer.
     (0 until gameZonePartitions).foreach { partition =>
-      apply(partition).run()
+      apply(partition)
+        .map { msg =>
+          println(msg)
+          msg
+        }
+        .run()
     }
   }
 }
