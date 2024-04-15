07691d6aeaa0d0ea58830e265370ca4acd5027b4
marcos <marcosrolando.mr@gmail.com>
2024-04-15 20:15:00 -0300

    Add debug config

--- run.sh
+++ run.sh
@@ -4,24 +4,33 @@ DEFAULT_AKKA_PORT=25520
 DEFAULT_PLAYER_ACCEPTER_PORT=2020
 
 show_help() {
-    echo "Usage: $0 [AKKA_PORT] [PLAYER_ACCEPTER_PORT]"
-    echo "ATTENTION! Run without arguments for first node execution!"
+    echo "Usage: $0 [AKKA_PORT] [PLAYER_ACCEPTER_PORT] [--debug, -d] [--help, -h]"
+    echo "ATTENTION! Run without port arguments for first node execution!"
     echo "  AKKA_PORT: Port for Akka communication (default: $DEFAULT_AKKA_PORT)"
     echo "  PLAYER_ACCEPTER_PORT: Port for player accepter (default: $DEFAULT_PLAYER_ACCEPTER_PORT)"
+    echo "  --debug: Enable JVM debug mode on port 5005"
     echo "  --help, -h: Show this help message"
     exit 0
 }
+
+ports=() # For removing -h or -d options from the command args
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --help | -h)
             show_help
             ;;
+        --debug | -d)
+            debug_option="-jvm-debug 5005"
+            ;;
         *)
-            break
+            ports+=("$1")
             ;;
     esac
+    shift # Remove option from command args
 done
+
+set -- "${ports[@]}"
 
 akka_port=${1:-$DEFAULT_AKKA_PORT}
 player_accepter_port=${2:-$DEFAULT_PLAYER_ACCEPTER_PORT}
@@ -42,4 +51,5 @@ sbt \
   -Dakka.remote.artery.canonical.port=$akka_port \
   -Dakka.remote.artery.bind.port=$akka_port \
   -Dgame.player-accepter.port=$player_accepter_port \
+  $debug_option \
   run
