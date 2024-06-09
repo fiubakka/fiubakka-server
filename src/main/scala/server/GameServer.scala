879da5fa06c04b5d8263c4a842b67e555854a3d9
marcos <marcosrolando.mr@gmail.com>
2024-06-09 12:09:55 -0300

    Refactor consumer and producer instantiation

--- src/main/scala/server/GameServer.scala
+++ src/main/scala/server/GameServer.scala
@@ -2,31 +2,24 @@ package server
 import akka.actor.typed.Behavior
 import akka.actor.typed.scaladsl.Behaviors
 import server.protocol.client.PlayerAccepter
-import server.protocol.event.kafka.KafkaConsumer
-import server.protocol.event.kafka.KafkaProducer
 
 object GameServer {
   sealed trait Command
   final case class Run() extends Command
 
   def apply(): Behavior[Command] = {
-    Behaviors.setup { ctx =>
-      KafkaProducer.configure(ctx.system)
-      KafkaConsumer.configure(ctx)
+    Behaviors.receive((ctx, msg) => {
+      msg match {
+        case Run() => {
+          println("Game server is running...")
+          ctx.spawn(PlayerAccepter(), "PlayerAccepter")
 
-      Behaviors.receive((ctx, msg) => {
-        msg match {
-          case Run() => {
-            println("Game server is running...")
-            ctx.spawn(PlayerAccepter(), "PlayerAccepter")
+          // println("Spawning bots...")
+          // (1 to 2).foreach(i => ctx.spawn(Bot(), s"Bot$i"))
+          Behaviors.same
 
-            // println("Spawning bots...")
-            // (1 to 2).foreach(i => ctx.spawn(Bot(), s"Bot$i"))
-            Behaviors.same
-
-          }
         }
-      })
-    }
+      }
+    })
   }
 }
