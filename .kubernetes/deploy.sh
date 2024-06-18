95de8ed6d673f9d93d1377837ba0a6fbed89864c
marcos <marcosrolando.mr@gmail.com>
2024-06-17 22:18:32 -0300

    Fix some cinnamon shit (wip)

--- .kubernetes/deploy.sh
+++ .kubernetes/deploy.sh
@@ -3,11 +3,15 @@
 NAMESPACE="fiubakka-server-1"
 KAFKA_RELEASE_NAME="fiubakka-kafka"
 POSTGRES_RELEASE_NAME="fiubakka-postgres"
+PROMETHEUS_RELEASE_NAME="fiubakka-prometheus"
+GRAFANA_RELEASE_NAME="fiubakka-grafana"
 
 if ! kubectl get namespace "$NAMESPACE" &> /dev/null; then
     echo "Creating namespace '$NAMESPACE'."
     kubectl create namespace "$NAMESPACE"
 fi
+
+kubectl apply -f .kubernetes/grafana-cinnamon.yaml -n "$NAMESPACE"
 
 if ! helm list -n "$NAMESPACE" | grep -q "$KAFKA_RELEASE_NAME"; then
     echo "Installing Helm release '$KAFKA_RELEASE_NAME' in namespace '$NAMESPACE'."
@@ -26,6 +30,21 @@ if ! helm list -n "$NAMESPACE" | grep -q "$POSTGRES_RELEASE_NAME"; then
         "$POSTGRES_RELEASE_NAME" oci://registry-1.docker.io/bitnamicharts/postgresql \
         --set auth.enablePostgresUser=true \
         --set auth.postgresPassword=postgres \
+        --namespace "$NAMESPACE"
+fi
+
+if ! helm list -n "$NAMESPACE" | grep -q "$PROMETHEUS_RELEASE_NAME"; then
+    echo "Installing Helm release '$PROMETHEUS_RELEASE_NAME' in namespace '$NAMESPACE'."
+    helm install \
+        "$PROMETHEUS_RELEASE_NAME" prometheus-community/prometheus \
+        --namespace "$NAMESPACE"
+fi
+
+if ! helm list -n "$NAMESPACE" | grep -q "$GRAFANA_RELEASE_NAME"; then
+    echo "Installing Helm release '$GRAFANA_RELEASE_NAME' in namespace '$NAMESPACE'."
+    helm install \
+        "$GRAFANA_RELEASE_NAME" grafana/grafana \
+        -f .kubernetes/helm/grafana.yaml \
         --namespace "$NAMESPACE"
 fi
 
